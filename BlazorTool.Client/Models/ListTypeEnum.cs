5fc48c6de83e96abac38fb6cf4f00bf2d878d76f
Yury Filionchyk <yury.filionchyk@d-tech.com.pl>
2025-07-02 15:48:51 +0200

    Add filter WO_state OrdersPage fix logic of filtering OrdersPage add labels for filters

---
+++ BlazorTool.Client/Models/ListTypeEnum.cs
@@ -0,0 +1,11 @@
+namespace BlazorTool.Client.Models
+{
+    public enum ListTypeEnum
+    {
+        Category = 1,
+        Level = 2,
+        Reason = 3,
+        State = 4,
+        Department = 5
+    }
+}
